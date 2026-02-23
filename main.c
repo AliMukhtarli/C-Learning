a010ad24c2cdbe251c18c01961d1529c8e8d0851
Ali Mukhtarli <alimukhtarli085@gmail.com>
2026-02-23 12:46:30 +0400

    YEnilikler

--- main.c
+++ main.c
@@ -4,6 +4,17 @@ int main(){
 
     printf("Hello World!!!\n");
     printf("C biz gelluk");
+
+    float num;
+    printf("Pleas enter a number:  ");
+    scanf("%f", &num);
+
+    if (num > 0)
+        printf("%f is a positive number. /n" , num);
+    else if (num == 0)
+        printf ("%f is a zero. /n", num);
+    else
+        printf("%f is a negative number./n", num);
     return 0;
 
 }
