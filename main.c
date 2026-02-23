2f56def2c974fb594194dc582bb31c9c88b68aa6
Ali Mukhtarli <alimukhtarli085@gmail.com>
2026-02-23 14:52:35 +0400

    Algoritmik dusunce C-de

--- main.c
+++ main.c
@@ -31,9 +31,21 @@ int main(){
 
     // type casting
 
-    int a = 5, b = 2;
+    /*int a = 5, b = 2;
     float c = (float) a/b;
     printf("%.1f\n", c);
-    return 0;
+    return 0;*/
+
+    for(int i = 1; i <= 100; i++){
+        if(i%3 == 0 && i % 5 == 0)
+            printf("%d: FizzBuzz \n", i);
+     else if(i%3 == 0)
+            printf("%d: Fizz \n", i);
+     else if (i%5 == 0)
+        printf("%d: Buzz \n", i);
+     else
+        printf("%d \n", i);
+
+    }
 
 }
